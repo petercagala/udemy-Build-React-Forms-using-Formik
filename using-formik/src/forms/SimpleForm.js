2802c4fc817decdd7190391230fc8a0e64775ffd
Peter Čagala <petercagala@gmail.com>
2020-05-27 11:17:23 +0200

    [9-1] Refactoring: rended not in the body of Formik, but in attribute, use ErrorMessage component

--- using-formik/src/forms/SimpleForm.js
+++ using-formik/src/forms/SimpleForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Formik} from 'formik';
+import {Formik, ErrorMessage} from 'formik';
 
 const SimpleForm = () => {
 
@@ -22,60 +22,47 @@ const SimpleForm = () => {
             }
             }
             validate={validationFunction}
-        >
-            {
-                ({
-                     handleSubmit,
-                     handleChange,
-                     values,
-                     errors,
-                     handleBlur, // s touto funkciou sa vyhodnoti napr. validate,
-                     // aj ked do pola nic nenapises, len z neho odides
-                     touched,
-                    isSubmitting,
-                 }) => (
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type={"text"}
-                            name={"firstName"}
-                            onChange={handleChange}
-                            value={values.firstName}
-                            placeholder={"Enter your firstName"}
-                            onBlur={handleBlur}
-                        />
-                        <input
-                            type={"text"}
-                            name={"lastName"}
-                            onChange={handleChange}
-                            value={values.lastName}
-                            placeholder={"Enter your lastName"}
-                            onBlur={handleBlur}
-                        />
+            render={({
+                         handleSubmit,
+                         handleChange,
+                         values,
+                         errors,
+                         handleBlur, // s touto funkciou sa vyhodnoti napr. validate,
+                         // aj ked do pola nic nenapises, len z neho odides
+                         touched,
+                         isSubmitting,
+                     }) => (
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type={"text"}
+                        name={"firstName"}
+                        onChange={handleChange}
+                        value={values.firstName}
+                        placeholder={"Enter your firstName"}
+                        onBlur={handleBlur}
+                    />
+                    <input
+                        type={"text"}
+                        name={"lastName"}
+                        onChange={handleChange}
+                        value={values.lastName}
+                        placeholder={"Enter your lastName"}
+                        onBlur={handleBlur}
+                    />
 
-                        {
-                            // Krasne nastavime v onSubmit: setSubmitting, ci sa ma pocas volania nejakeho requestu
-                            // dalej zobrazovat button
-                        }
-                        <button disabled={isSubmitting}>Submit</button>
+                    {
+                        // Krasne nastavime v onSubmit: setSubmitting, ci sa ma pocas volania nejakeho requestu
+                        // dalej zobrazovat button
+                    }
+                    <button disabled={isSubmitting}>Submit</button>
 
-                        {
-                            errors.firstName && (
-                                <div style={{color: 'red'}}>
-                                    {errors.firstName}
-                                </div>
-                            )
-                        }
-                        {
-                            errors.lastName && (
-                                <div style={{color: 'blue'}}>
-                                    {errors.lastName}
-                                </div>
-                            )
-                        }
-                    </form>
-                )
-            }
-        </Formik>
+                    <br/>
+                    <ErrorMessage name={"firstName"}/>
+                    <br/>
+                    <ErrorMessage name={"lastName"}/>
+                </form>
+            )}
+        />
     );
 }
 
