2f1eed3e2ce51d9d12a4a8923df8f1b4d89730eb
Peter Čagala <petercagala@gmail.com>
2020-05-27 10:42:09 +0200

    [6-1] Form validation

--- using-formik/src/forms/SimpleForm.js
+++ using-formik/src/forms/SimpleForm.js
@@ -14,11 +14,13 @@ const SimpleForm = () => {
                 console.log(values);
                 }
             }
+            validate={validationFunction}
         >
             {
                 ({ handleSubmit,
                      handleChange,
-                     values }) => (
+                     values,
+                     errors}) => (
                      <form onSubmit={handleSubmit}>
                          <input
                              type={"text"}
@@ -36,11 +38,45 @@ const SimpleForm = () => {
                          />
 
                          <button>Submit</button>
+
+                         {
+                             errors.firstName && (
+                                 <div style={{color: 'red'}}>
+                                     {errors.firstName}
+                                 </div>
+                             )
+                         }
+                         {
+                             errors.lastName && (
+                                 <div style={{color: 'blue'}}>
+                                     {errors.lastName}
+                                 </div>
+                             )
+                         }
                      </form>
                 )
             }
         </Formik>
     );
+}
+
+const validationFunction = (values) => {
+    console.log("cagalpte validationFunction");
+    console.log(values);
+
+    let errorsObject = {
+
+    };
+
+    if(!values.firstName) {
+        errorsObject.firstName = "Please, enter firstName";
+    }
+
+    if(!values.lastName) {
+        errorsObject.lastName = "Please, enter lastName";
+    }
+
+    return errorsObject;
 }
 
 export default SimpleForm;
